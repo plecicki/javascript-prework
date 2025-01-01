e0cb3fd51875da8b4f4035b1717107016314f599
Piotr Lecicki <p.lecicki@gmail.com>
2025-01-01 13:03:10 +0100

    Add more computer moves possibilities

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! ' + 'Twój ruch to ' + playerMove + "! Dobra robota!");
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
